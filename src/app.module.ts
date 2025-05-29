ae887aebdea85fd651b4b7329f86b66e56ba8699
Diego Yepes <diego.yepes.dev.05@gmail.com>
2025-05-29 17:13:54 -0500

    Hello World

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GeminiModule } from './gemini/gemini.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [GeminiModule],
-  controllers: [],
+  imports: [
+    GeminiModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/gemini/gemini.module.ts
+++ src/gemini/gemini.module.ts
@@ -1,7 +1,4 @@
-import { join } from 'path';
-
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { GeminiService } from './gemini.service';
 import { GeminiController } from './gemini.controller';
@@ -9,10 +6,5 @@ import { GeminiController } from './gemini.controller';
 @Module({
   controllers: [GeminiController],
   providers: [GeminiService],
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-  ],
 })
 export class GeminiModule {}
